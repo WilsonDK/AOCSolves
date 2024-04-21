eda2786741855c685281f2a781871329fbe067df
WilsonDK <31414284+WilsonDK@users.noreply.github.com>
2024-04-21 17:17:40 -0500

    Create AOC2023-Day1-Solved.ps1
    First Puzzle Solved Yeah!
    Resources Used for string to int check:
    https://stackoverflow.com/questions/25322636/powershell-convert-string-to-number

---
+++ Powershell/AOC2023-Day1-Solved.ps1
@@ -0,0 +1,21 @@
+﻿$path = #Insert FilePath
+$Input = Get-Content $path
+$Output = @()
+
+for($inputCounter = 0; $inputCounter -lt $input.Count; $inputCounter++){
+$parsed = $input[$inputCounter]
+$intergers = @()
+    for($charCounter = 0;$charCounter -lt $parsed.Length; $charCounter++){
+    #write-host $parsed[$charCounter]
+    $returnedInterger=0
+        $charParsed = $parsed[$charCounter]
+        if([int]::TryParse($charParsed,[ref]$returnedInterger)){
+        $intergers += $returnedInterger
+        }
+    }
+    $processStringtoInt = "$($intergers[0])$($intergers[$intergers.Length-1])"
+    $ArrayOutput += [int]$processStringtoInt
+}
+
+write-host $ArrayOutput
+Clear-Variable  ArrayOutput
